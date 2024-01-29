cae8842e0088f388be1b802f9825b1d213a32cd6
Andrii <andrejsalogub3@gmail.com>
2024-01-29 16:44:55 +0200

    Added drawing functionality to the canvas: color setting and mouse event handling.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,33 @@
+const currentColor = "#000";
 
+const canvas = document.querySelector(".drawing-panel__canvas");
+const canvasSizeInput = document.querySelector(".drawing-panel__size-changer");
+let isMouseDown = false;
+
+changeInputSize(canvasSizeInput.value);
+
+function changeInputSize(size) {
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        canvas.append(div);
+    }
+}
+
+canvas.addEventListener("mousedown", handleMouseDown);
+canvas.addEventListener("mouseup", handleMouseUp);
+canvas.addEventListener("mouseover", handleMouseOver);
+
+function handleMouseDown(e) {
+    e.target.style.background = currentColor;
+    isMouseDown = true;
+}
+
+function handleMouseUp() {
+    isMouseDown = false;
+}
+
+function handleMouseOver(e) {
+    if (isMouseDown) {
+        e.target.style.background = currentColor;
+    }
+}
