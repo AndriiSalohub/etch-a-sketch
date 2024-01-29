6381b03d1248445383cefea8ee88db4b67f404f0
Andrii <andrejsalogub3@gmail.com>
2024-01-29 17:02:01 +0200

    Added color picker and clear button functionality to the drawing canvas.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
-const currentColor = "#000";
+let currentColor = "#000";
 
 const canvas = document.querySelector(".drawing-panel__canvas");
 const canvasSizeInput = document.querySelector(".drawing-panel__size-changer");
+const colorPickerInput = document.querySelector(".drawing-panel__color-picker");
+const clearBtn = document.querySelector("#clear-btn");
+
 let isMouseDown = false;
 
 changeInputSize(canvasSizeInput.value);
@@ -17,6 +20,10 @@ canvas.addEventListener("mousedown", handleMouseDown);
 canvas.addEventListener("mouseup", handleMouseUp);
 canvas.addEventListener("mouseover", handleMouseOver);
 
+colorPickerInput.addEventListener("input", handleColorPick);
+
+clearBtn.addEventListener("click", handleClearBtnClick);
+
 function handleMouseDown(e) {
     e.target.style.background = currentColor;
     isMouseDown = true;
@@ -30,4 +37,14 @@ function handleMouseOver(e) {
     if (isMouseDown) {
         e.target.style.background = currentColor;
     }
+}
+
+function handleColorPick(e) {
+    currentColor = e.target.value;
+}
+
+function handleClearBtnClick() {
+    Array.from(canvas.children).forEach((child) => {
+        child.style.background = "#fff";
+    });
 }
