7bd226fe34cec32435c91a9a58556a423392dffc
Andrii <andrejsalogub3@gmail.com>
2024-01-30 14:04:14 +0200

    Implemented canvas resizing feature.

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ const canvas = document.querySelector(".drawing-panel__canvas");
 const canvasSizeInput = document.querySelector(".drawing-panel__size-changer");
 const colorPickerInput = document.querySelector(".drawing-panel__color-picker");
 const clearBtn = document.querySelector("#clear-btn");
+const sizeText = document.querySelector(".drawing-panel__size-text");
 
 let isMouseDown = false;
 
@@ -20,9 +21,13 @@ canvas.addEventListener("mousedown", handleMouseDown);
 canvas.addEventListener("mouseup", handleMouseUp);
 canvas.addEventListener("mouseover", handleMouseOver);
 
+canvasSizeInput.addEventListener("input", handleSizeChange);
+
 colorPickerInput.addEventListener("input", handleColorPick);
 
-clearBtn.addEventListener("click", handleClearBtnClick);
+clearBtn.addEventListener("click", () => {
+    clearAllCells();
+});
 
 function handleMouseDown(e) {
     e.target.style.background = currentColor;
@@ -43,8 +48,17 @@ function handleColorPick(e) {
     currentColor = e.target.value;
 }
 
-function handleClearBtnClick() {
+function clearAllCells() {
     Array.from(canvas.children).forEach((child) => {
         child.style.background = "#fff";
     });
+}
+
+function handleSizeChange(e) {
+    clearAllCells();
+    const size = e.target.value;
+    changeInputSize(size);
+    sizeText.textContent = `${size} x ${size}`;
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
